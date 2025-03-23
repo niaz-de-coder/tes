0069804d1528715fc5cdbc01cc95363fd75759c0
Md Niaz Al Motin <niaz.al.motin@gmail.com>
2025-03-23 06:46:07 -0700

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.getElementById('logo');
+    if (logo) {
+        logo.addEventListener('click', function() {
+            window.location.href = 'index.html';
+        });
+    }
+});
